c03b44678bc7b1229e6bda6a8f6f6f072ca83522
YoungYong Park <youngyongpark@gmail.com>
2014-10-26 12:00:18 +0900

    Makes instance attributes to property.

--- engine/engine.py
+++ engine/engine.py
@@ -11,8 +11,30 @@ from .view import View as dpView
 
 
 class Engine(object):
-    def __init__(self):
-        self.config = dpConfig()
-        self.model = dpModel()
-        self.helper = dpHelper()
-        self.view = dpView()
+    @property
+    def config(self):
+        if not hasattr(self, '_config'):
+            self._config = dpConfig()
+
+        return self._config
+
+    @property
+    def model(self):
+        if not hasattr(self, '_model'):
+            self._model = dpModel()
+
+        return self._model
+
+    @property
+    def helper(self):
+        if not hasattr(self, '_helper'):
+            self._helper = dpHelper()
+
+        return self._helper
+
+    @property
+    def view(self):
+        if not hasattr(self, '_view'):
+            self._view = dpView()
+
+        return self._view

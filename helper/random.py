6e367d027bd37d8bf586bd7eda06d5e2c78faadc
YoungYong Park <youngyongpark@gmail.com>
2014-10-30 16:04:45 +0900

    Implement random helper.

---
+++ helper/random.py
@@ -0,0 +1,19 @@
+#
+#	dp for Tornado
+#		YoungYong Park (youngyongpark@gmail.com)
+#		2014.10.23
+#
+
+
+from __future__ import absolute_import
+from engine.helper import Helper as dpHelper
+
+import random
+
+
+class RandomHelper(dpHelper):
+    def randint(self, a, b):
+        return random.randint(a, b)
+
+    def choice(self, seq):
+        return random.choice(seq)

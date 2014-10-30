19999f571e75105245b02cab339e7d7fad3a0433
YoungYong Park <youngyongpark@gmail.com>
2014-10-30 16:04:26 +0900

    Add datetime helper functions.

--- helper/datetime.py
+++ helper/datetime.py
@@ -16,14 +16,14 @@ class DatetimeHelper(dpHelper):
     def current_time(self):
         return int(time.time())
 
-    def time(self):
-        return self.current_time()
+    def time(self, dt=None):
+        return self.current_time() if datetime is None else self.timestamp_from_datetime(dt, False)
 
     def current_time_millis(self):
         return int(round(time.time() * 1000))
 
-    def mtime(self):
-        return self.current_time_millis()
+    def mtime(self, dt=None):
+        return self.current_time_millis() if datetime is None else self.timestamp_from_datetime(dt, True)
 
     def today(self):
         return datetime.datetime.today()
@@ -50,8 +50,23 @@ class DatetimeHelper(dpHelper):
     def secs_to_next_hour(self, from_time=None):
         return int(self.millisecs_to_next_hour(from_time=from_time) / 1000)
 
+    def millisecs_to_tomorrow(self, from_time=None):
+        dst = self.today() + self.timedelta(days=1)
+        from_time = from_time if from_time is not None else self.current_time
+
+        return self.mktime(dst.year, dst.month, dst.day, 0, 0, 0) - from_time
+
+    def secs_to_tomorrow(self, from_time=None):
+        return int(self.millisecs_to_tomorrow(from_time=from_time) / 1000)
+
     def timedelta(self, days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
         return datetime.timedelta(days, seconds, microseconds, milliseconds, minutes, hours, weeks)
 
     def mktime(self, year=1970, month=1, day=1, hour=0, mins=0, sec=0):
         return int(time.mktime((year, month, day, hour, mins, sec, 0, 0, 0)))
+
+    def timestamp_from_datetime(self, dt, millisecs=False):
+        if not isinstance(dt, datetime.datetime):
+            return None
+
+        return int(dt.timestamp() * (1000 if millisecs else 1))

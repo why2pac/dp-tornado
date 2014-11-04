ff3e0eb9d38d2cbed1fab7b67a374915bf65b8f5
YoungYong Park <youngyongpark@gmail.com>
2014-11-04 16:16:06 +0900

    Add logging helper. (exception, error, warning, info, debug)

--- engine/logger.py
+++ engine/logger.py
@@ -4,9 +4,24 @@
 #		2014.10.23
 #		
 
+
+import logging
+
 from .singleton import Singleton
 
 
 class Logger(object, metaclass=Singleton):
-    def exception(self, e=None):
-        pass
+    def exception(self, msg, *args, **kwargs):
+        logging.exception(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        logging.error(msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        logging.info(msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        logging.warning(msg, *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        logging.debug(msg, *args, **kwargs)

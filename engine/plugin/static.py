f0fd709ba5a392e3e21978ed60182f161b2ed0ae
YoungYong Park <youngyongpark@gmail.com>
2014-11-26 11:57:41 +0900

    Add static file URL hash.

--- engine/plugin/static.py
+++ engine/plugin/static.py
@@ -7,8 +7,13 @@
 
 import tornado.web
 
+from engine.model import InValueModelConfig as dpInValueModelConfig
+
 
 class StaticURL(tornado.web.UIModule):
+    compiled = {}
+    cache_config = dpInValueModelConfig(driver='sqlite', database='static_url_compiled', pure=True)
+
     def render(self, *static_urls, **options):
         html = []
 
@@ -25,6 +30,22 @@ class StaticURL(tornado.web.UIModule):
                 raise NotImplementedError
 
             static_path = '%s%s' % (self.handler.settings.get('static_url_prefix'), path)
+
+            if static_path not in StaticURL.compiled:
+                cache_key = '%s-%s' % (static_path, self.handler.application.startup_at)
+                cached = self.handler.cache.get(cache_key, dsn_or_conn=StaticURL.cache_config)
+
+                if not cached:
+                    hash = self.handler.helper.datetime.current_time()
+                    hash = self.handler.helper.crypto.md5_hash('%s / %s' % (static_path, hash))
+                    StaticURL.compiled[static_path] = hash
+
+                    self.handler.cache.set(cache_key, hash, dsn_or_conn=StaticURL.cache_config)
+
+                else:
+                    StaticURL.compiled[static_path] = cached
+
+            static_path = '%s?%s' % (static_path, StaticURL.compiled[static_path])
             html.append(template % {'url': static_path})
 
         return '\n'.join(html)

76a1c36965313c6e9174e9979c396dbcf104c8db
YoungYong Park <youngyongpark@gmail.com>
2014-12-12 16:20:08 +0900

    Add ui_methods for datetime manipulation.

--- engine/plugin/ui_methods.py
+++ engine/plugin/ui_methods.py
@@ -10,3 +10,9 @@ def trim(c, t):
 
 def nl2br(c, t):
     return t.replace('\r\n', '<br />').replace('\r', '<br />').replace('\n', '<br />')
+
+def yyyymmdd(c, t, s='.'):
+    return c.helper.datetime.yyyymmdd(s=s, d=t)
+
+def hhiiss(c, t, s=':'):
+    return c.helper.datetime.hhiiss(s=s, d=t)

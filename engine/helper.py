50dfcf86b520451d165f7994e699bdc1035e0cdf
YoungYong Park <youngyongpark@gmail.com>
2014-12-18 17:31:00 +0900

    Helper inherits from dpLoader.

--- engine/helper.py
+++ engine/helper.py
@@ -8,7 +8,8 @@
 
 from .singleton import Singleton as dpSingleton
 from .engine import Engine as dpEngine
+from .loader import Loader as dpLoader
 
 
-class Helper(dpEngine, dpSingleton):
+class Helper(dpEngine, dpLoader, dpSingleton):
     pass

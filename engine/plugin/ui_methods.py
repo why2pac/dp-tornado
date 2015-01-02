207ef39382f73ae363231bdb8772e0e9e5740889
YoungYong Park <youngyongpark@gmail.com>
2015-01-02 12:56:59 +0900

    nl2br, make more safety.

--- engine/plugin/ui_methods.py
+++ engine/plugin/ui_methods.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #   dp for Tornado
-#      YoungYong Park (youngyongpark@gmail.com)
-#      2014.11.21
+#     YoungYong Park (youngyongpark@gmail.com)
+#     2014.11.21
 #
+
+
+import tornado.escape
 
 
 def trim(c, t):
     return t.strip()
 
-def nl2br(c, t):
+def nl2br(c, t, escape=True):
+    t = tornado.escape.xhtml_escape(t) if escape else t
     return t.replace('\r\n', '<br />').replace('\r', '<br />').replace('\n', '<br />')
 
 def yyyymmdd(c, t, s='.'):

5df5de3efe3f9c4b8794752822e61009c775d2e0
Why Park <youngyongpark@gmail.com>
2015-01-20 00:01:53 +0900

    Add print request_uri helper.

--- engine/plugin/ui_methods.py
+++ engine/plugin/ui_methods.py
@@ -12,6 +12,7 @@ import tornado.escape
 def trim(c, t):
     return t.strip()
 
+
 def nl2br(c, t, escape=True):
     if not t:
         return ''
@@ -19,8 +20,14 @@ def nl2br(c, t, escape=True):
     t = tornado.escape.xhtml_escape(t) if escape else t
     return t.replace('\r\n', '<br />').replace('\r', '<br />').replace('\n', '<br />')
 
+
 def yyyymmdd(c, t, s='.'):
     return c.helper.datetime.yyyymmdd(s=s, d=t)
 
+
 def hhiiss(c, t, s=':'):
     return c.helper.datetime.hhiiss(s=s, d=t)
+
+
+def request_uri(c, s=False, d=' ', p='_'):
+    return ('%s%s' % (d, p)).join(c.request.uri.split('/')).strip() if s else c.request.uri

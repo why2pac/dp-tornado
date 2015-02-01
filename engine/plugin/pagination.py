9dea568ca7ab8663f961f3b8c2cdaddc205d6382
Why Park <youngyongpark@gmail.com>
2015-02-02 00:12:51 +0900

    Add more options for pagination.

--- engine/plugin/pagination.py
+++ engine/plugin/pagination.py
@@ -26,14 +26,18 @@ class Pagination(tornado.web.UIModule, dpEngine):
 
     link_tag = 'a'
     link_class = ''
+    link_prefix = ''
 
     space = ''
 
     def render(self, total_count, page, rpp, **options):
         rpb = options['rpb'] if 'rpb' in options else 10
+        current_tag = options['current_tag'] if 'current_tag' in options else self.current_tag
+        current_class = options['current_class'] if 'current_class' in options else self.current_class
         space = options['space'] if 'space' in options else self.space
-        prev = options['prev'] if 'prev' in options else self.prev
-        next = options['next'] if 'next' in options else self.next
+        prev_btn = options['prev'] if 'prev' in options else self.prev
+        next_btn = options['next'] if 'next' in options else self.next
+        link_prefix = options['link_prefix'] if 'link_prefix' in options else self.link_prefix
 
         uri = self.helper.url.parse(self.request)
         url = uri.path
@@ -59,28 +63,35 @@ class Pagination(tornado.web.UIModule, dpEngine):
         if current_block > 1:
             params['page'] = (current_block - 1) * rpb
             s = self.helper.url.build(url, params)
-            output = ('%s<%s href="%s" class=%s">%s</%s>%s'
-                      % (output, self.link_tag, s, self.prev_class, prev, self.link_tag, space))
+            output = ('%s<%s href="%s%s" class=%s">%s</%s>%s'
+                      % (output, self.link_tag, link_prefix, s, self.prev_class, prev_btn, self.link_tag, ''))
 
         # Paging
-        for i in range(((current_block - 1) * rpb) + 1, (current_block * rpb) + 1):
+        ra = ((current_block - 1) * rpb) + 1
+        rb = (current_block * rpb) + 1
+
+        for i in range(ra, rb):
             if i < 1:
                 continue
             elif i > last_page:
                 break
 
-            if i != ((current_block - 1) * rpb) + 1:
-                output = '%s%s' % (output, space)
+            if ra == i:
+                _space = ''
+            else:
+                _space = space
+
+            output = '%s%s' % (output, _space)
 
             if page == i:
                 output = ('%s<%s class="%s">%s</%s>'
-                          % (output, self.current_tag, self.current_class, i, self.current_tag))
+                          % (output, current_tag, current_class, i, current_tag))
             else:
                 params['page'] = i
                 s = self.helper.url.build(url, params)
 
-                output = ('%s<%s%s href="%s">%s</%s>'
-                          % (output, self.link_tag, self.link_class, s, i, self.link_tag))
+                output = ('%s<%s%s href="%s%s">%s</%s>'
+                          % (output, self.link_tag, self.link_class, link_prefix, s, i, self.link_tag))
 
         # Next Button
         if current_block < last_block:
@@ -88,7 +99,7 @@ class Pagination(tornado.web.UIModule, dpEngine):
             params['page'] = last_page if params['page'] > last_page else params['page']
             s = self.helper.url.build(url, params)
 
-            output = ('%s%s<%s href="%s" class="%s">%s</%s>'
-                      % (output, space, self.link_tag, s, self.next_class, next, self.link_tag))
+            output = ('%s%s<%s href="%s%s" class="%s">%s</%s>'
+                      % (output, '', self.link_tag, link_prefix, s, self.next_class, next_btn, self.link_tag))
 
         return output

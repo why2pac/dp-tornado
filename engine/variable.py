9724170bb9d5cebed95b645616c367cb587301a0
Why Park <youngyongpark@gmail.com>
2015-03-07 19:19:42 +0900

    Fixed recursive assigning issue.

--- engine/variable.py
+++ engine/variable.py
@@ -7,29 +7,20 @@
 
 
 from .namer import Namer as dpNamer
-from .singleton import Singleton as dpSingleton
 
 
-class Variable(dpNamer, dpSingleton):
+class Variable(dpNamer):
     def __init__(self, *args, **kwargs):
         self.__dict__['_prepared'] = {}
 
     def __getattr__(self, name):
-        if name in self.__dict__['_prepared']:
-            return self.__dict__['_prepared'][name]
-        else:
-            return Variable()
+        if name not in self.__dict__['_prepared']:
+            self.__dict__['_prepared'][name] = Variable()
+
+        return self.__dict__['_prepared'][name]
 
     def __setattr__(self, key, value):
-        if isinstance(value, dict):
-            prepared = dpNamer()
-
-            for k in value.keys():
-                prepared.__setattr__(k, value[k])
-
-            self.__dict__['_prepared'][key] = prepared
-        else:
-            self.__dict__['_prepared'][key] = value
+        self.__dict__['_prepared'][key] = value
 
     def __iter__(self):
         return iter(self.__dict__['_prepared'])

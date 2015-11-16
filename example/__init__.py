f8392d82d9436196bfed6944616e64239186147e
YoungYong Park <youngyongpark@gmail.com>
2015-11-17 02:02:27 +0900

    Merge __init__ and bootstrap.

--- example/__init__.py
+++ example/__init__.py
@@ -3,7 +3,7 @@
 
 import os
 
-from dp_tornado.bootstrap import Bootstrap
+from dp_tornado import Bootstrap
 
 kwargs = {
     'application_path': os.path.join(os.path.dirname(os.path.realpath(__file__))),

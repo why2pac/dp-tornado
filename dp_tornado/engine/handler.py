3dc1b82691766cda4f104f67197c7e5ef502b128
Why Park <youngyongpark@gmail.com>
2015-11-22 11:38:45 +0900

    checking whether headers sent or not with _headers_written variable.

--- dp_tornado/engine/handler.py
+++ dp_tornado/engine/handler.py
@@ -246,10 +246,8 @@ class Handler(tornado.web.RequestHandler, dpEngine):
         self.__processor('put', path)
 
     def postprocess(self, x):
-        if hasattr(self, '__postprocessed__'):
+        if self._headers_written:
             return
-
-        setattr(self, '__postprocessed__', True)
 
         if not x:
             return

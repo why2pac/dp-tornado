1d5a185bc24166ee7d722e5bb511c3b53ed07518
Why Park <youngyongpark@gmail.com>
2015-11-27 06:22:02 +0900

    add cache clear example.

--- example/controller/test/case/cache_decorator.py
+++ example/controller/test/case/cache_decorator.py
@@ -5,7 +5,10 @@ from dp_tornado.engine.controller import Controller
 
 
 class CacheDecoratorController(Controller):
-    def get(self):
+    def get(self, mode=None):
+        if mode == 'clear':
+            self.model.foo.clear()
+
         self.write('cache_decorator_memory : ')
         self.write(str(self.model.foo.cache_decorator_memory(10, 20, 30)))
         self.write("<br />\n")
--- example/model/foo/__init__.py
+++ example/model/foo/__init__.py
@@ -5,10 +5,16 @@ from dp_tornado.engine.model import Model as dpModel
 
 
 class FooModel(dpModel):
+    def clear(self):
+        dpModel.cache_clear(self.cache_decorator_memory, 10, 20, 30)
+        dpModel.cache_clear(self.cache_decorator_local, 30, 20, 10)
+
     @dpModel.cache(10)
     def cache_decorator_memory(self, a, b, c):
+        print('new', 'cache_decorator_memory')
         return a+b+c+self.helper.random.randint(0, 10**7)
 
     @dpModel.cache('foo.bar/memory', 10)
     def cache_decorator_local(self, a, b, c):
+        print('new', 'cache_decorator_local')
         return a+b+c+self.helper.random.randint(0, 10**7)

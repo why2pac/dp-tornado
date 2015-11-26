518b525f2e459f5bcaab26fca019bb9cd570e248
Why Park <youngyongpark@gmail.com>
2015-11-27 06:27:31 +0900

    version updated, 0.2.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='dp-tornado',
-    version="0.2.1",
+    version="0.2.2",
     url='http://github.com/why2pac/dp-tornado',
     license='MIT',
     author='YoungYong Park',

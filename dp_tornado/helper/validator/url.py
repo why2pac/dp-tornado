299ad538f798203ed5a4c2d4d3b4f5fb93b32820
Why Park <youngyongpark@gmail.com>
2015-11-30 23:08:41 +0900

    add url helper.

---
+++ dp_tornado/helper/validator/url.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+from dp_tornado.engine.helper import Helper as dpHelper
+
+import re
+
+regex = re.compile(
+    r'^(?:http|ftp)s?://'
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+    r'localhost|'
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    r'(?::\d+)?'
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+
+class UrlHelper(dpHelper):
+    def validate(self, e):
+        return True if re.match(regex, e) else False

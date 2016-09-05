7461caeed4b63aa4a8c1b682843e864fec0f8ba3
Why Park <youngyongpark@gmail.com>
2016-09-06 00:07:27 +0900

    add run/wait and stop server test set.

---
+++ tests/__main__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+from . import server
+
+server.run_server()
+server.wait_server()
+
+
+
+server.stop_server()
---
+++ tests/server.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+import time
+import subprocess
+
+
+dp_testing_identifier = 'dp-tornado-testing-9x890203'
+
+
+def run_server():
+    stop_server()
+    subprocess.Popen(['python', 'example/__init__.py', dp_testing_identifier, '&'])
+
+
+def wait_server(timeout=3):
+    for e in range(timeout):
+        if server_pids():
+            return True
+
+        time.sleep(1)
+
+    assert False
+
+
+def server_pids():
+    pids = subprocess.Popen(['pgrep', '-f', dp_testing_identifier], stdout=subprocess.PIPE)
+    pids = pids.stdout.readlines()
+
+    return [e.replace('\n', '') for e in (pids if pids else [])]
+
+
+def stop_server():
+    for pid in server_pids():
+        subprocess.Popen(['kill', '-9', pid])

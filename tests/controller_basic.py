e633db9638e579437d3b2ce6406a07a89b506018
Why Park <youngyongpark@gmail.com>
2016-09-23 23:49:20 +0900

    add expected code assertion.

--- tests/controller_basic.py
+++ tests/controller_basic.py
@@ -5,20 +5,20 @@ from . import utils
 
 
 def get():
-    utils.expecting_text('get', '/', 'tests::get')
+    utils.expecting_text('get', '/', 'tests::get', 200)
 
 
 def post():
-    utils.expecting_text('post', '/', 'tests::post')
+    utils.expecting_text('post', '/', 'tests::post', 200)
 
 
 def put():
-    utils.expecting_text('put', '/', 'tests::put:expected', {'arg1': 'expected'})
+    utils.expecting_text('put', '/', 'tests::put:expected', 200, {'arg1': 'expected'})
 
 
 def delete():
-    utils.expecting_text('delete', '/expected1/expected2', 'tests::delete:expected1:expected2')
+    utils.expecting_text('delete', '/expected1/expected2', 'tests::delete:expected1:expected2', 200)
 
 
 def head():
-    utils.expecting_text('head', '/', '')
+    utils.expecting_text('head', '/', '', 200)
--- tests/utils.py
+++ tests/utils.py
@@ -8,25 +8,26 @@ import logging
 from . import consts
 
 
-def req(method, url, params=None, retry=3, retry_delay=1):
+def req(session, method, url, params=None, retry=3, retry_delay=1):
+    session = session if session else requests.Session()
     url = '%s%s' % (consts.dp_testing_path, url)
 
     for e in range(retry):
         try:
             if method == 'get':
-                o = requests.get(url, data=params)
+                o = session.get(url, data=params)
             elif method == 'post':
-                o = requests.post(url, data=params)
+                o = session.post(url, data=params)
             elif method == 'put':
-                o = requests.put(url, data=params)
+                o = session.put(url, data=params)
             elif method == 'delete':
-                o = requests.delete(url, data=params)
+                o = session.delete(url, data=params)
             elif method == 'head':
-                o = requests.head(url, data=params)
+                o = session.head(url, data=params)
             else:
                 raise Exception('Invalid method.')
 
-            return o
+            return session, o
 
         except Exception:
             time.sleep(retry_delay)
@@ -37,19 +38,22 @@ def req(method, url, params=None, retry=3, retry_delay=1):
     assert False
 
 
-def req_text(method, url, params=None, retry=3, retry_delay=1):
-    o = req(url=url, method=method, params=params, retry=retry, retry_delay=retry_delay)
-    return o.text
+def req_text(method, url, params=None, retry=3, retry_delay=1, session=None):
+    session, o = req(
+        session=session, url=url, method=method, params=params, retry=retry, retry_delay=retry_delay)
+
+    return session, o.status_code, o.text
 
 
-def expecting_text(method, url, expected, params=None, retry=3, retry_delay=1):
-    response = req_text(url=url, method=method, params=params, retry=retry, retry_delay=retry_delay)
+def expecting_text(method, url, expected, expect_code=200, params=None, retry=3, retry_delay=1, session=None):
+    session, status_code, response = req_text(
+        url=url, method=method, params=params, retry=retry, retry_delay=retry_delay, session=session)
 
-    if response == expected:
-        return True
+    if status_code == expect_code and (expected is None or response == expected):
+        return session, True
 
     logging.error('URL : [%s] %s' % (method.upper(), url))
-    logging.error('Expected : %s' % expected)
-    logging.error('Resulting : %s' % response)
+    logging.error('Expected : %s, %s' % (expect_code, expected))
+    logging.error('Resulting : %s, %s' % (status_code, response))
 
     assert False

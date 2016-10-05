4c5ffec2dfc38f1fb3322ccb5d822792cf3f5a76
Why Park <youngyongpark@gmail.com>
2016-10-06 02:05:32 +0900

    support python 2 and 3 both.

--- dp_tornado/engine/decorators.py
+++ dp_tornado/engine/decorators.py
@@ -2,6 +2,7 @@
 
 
 import logging
+import sys
 
 from .singleton import Singleton as dpSingleton
 from functools import wraps
@@ -20,7 +21,11 @@ class DeprecatedDecorator(object):
     def __call__(self, func):
         @wraps(func)
         def wrapped_func(*args, **kwargs):
-            logging.warning('** This method has been deprecated > %s **' % func.__qualname__)
+            if sys.version_info[0] >= 3:
+                logging.warning('** This method has been deprecated > %s **' % func.__qualname__)
+            else:
+                logging.warning('** This method has been deprecated > %s.%s **' % (func.__module__, func.__name__))
+
             return func(*args, **kwargs)
 
         return wrapped_func

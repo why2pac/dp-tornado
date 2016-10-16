f2572ef765c1d1804067c2cecafec1cff0f09d44
Parker Park <me@dp.farm>
2016-10-17 03:05:14 +0900

    refactoring and auto typecase

--- dp_tornado/helper/security/crypto/hash.py
+++ dp_tornado/helper/security/crypto/hash.py
@@ -9,13 +9,19 @@ import hashlib
 
 class HashHelper(dpHelper):
     def md5(self, plain):
-        if not self.helper.string.is_string(plain):
+        if self.helper.misc.type.check.numeric(plain):
+            plain = str(plain)
+
+        if not self.helper.misc.type.check.string(plain):
             raise Exception('The specified value is not plain text.')
 
         return hashlib.md5(str(plain).encode()).hexdigest()
 
     def sha224(self, plain):
-        if not self.helper.string.is_string(plain):
+        if self.helper.misc.type.check.numeric(plain):
+            plain = str(plain)
+
+        if not self.helper.misc.type.check.string(plain):
             raise Exception('The specified value is not plain text.')
 
         return hashlib.sha224(str(plain).encode()).hexdigest()

21627a6c963b74c796046104668755d1b0e51da3
Parker Park <me@dp.farm>
2016-10-18 20:18:39 +0900

    add nginx consts.

--- tests/consts.py
+++ tests/consts.py
@@ -5,3 +5,6 @@ dp_testing_identifier = 'dp-tornado-testing-9x890203'
 dp_testing_port = '18923'
 dp_testing_host = 'http://127.0.0.1:%s' % dp_testing_port
 dp_testing_path = '%s/tests' % dp_testing_host
+
+dp_testing_nginx_port = '28923'
+dp_testing_nginx_host = 'http://127.0.0.1:%s' % dp_testing_nginx_port

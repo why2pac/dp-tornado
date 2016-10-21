1cd16bf66911299e75f37c496026ffcd912e32a7
Parker Park <me@dp.farm>
2016-10-21 15:49:21 +0900

    #3, datetime.time() -> datetime.now()

--- dp_tornado/engine/cache.py
+++ dp_tornado/engine/cache.py
@@ -457,7 +457,7 @@ class LockDecorator(object):
 
         self._dsn = dsn
         self._expire_in = expire_in
-        self._expire_at = _engine_.helper.datetime.timestamp.time() + expire_in
+        self._expire_at = _engine_.helper.datetime.timestamp.now() + expire_in
         self._func_name = None
 
     def __call__(self, f):
@@ -533,7 +533,7 @@ class Decorator(object):
             elif not cached:
                 continue
 
-            elif cached['exp'] and _engine_.datetime.timestamp.time() > cached['exp']:  # Value expired
+            elif cached['exp'] and _engine_.datetime.timestamp.now() > cached['exp']:  # Value expired
                 if not dsn:
                     delattr(_cached_, cache_key)
 
@@ -555,7 +555,7 @@ class Decorator(object):
 
     def _cache(self, cache_key, value):
         payload = {
-            'exp': _engine_.datetime.timestamp.time() + self._expire_in if self._expire_in else None,
+            'exp': _engine_.datetime.timestamp.now() + self._expire_in if self._expire_in else None,
             'val': value
         }
 
@@ -579,7 +579,7 @@ class Decorator(object):
         identifier = self._cached(identifier_key) if not ident_renew else None
 
         if not identifier:
-            identifier = _engine_.datetime.timestamp.time(ms=True)
+            identifier = _engine_.datetime.timestamp.now(ms=True)
             self._cache(identifier_key, identifier)
         else:
             identifier = identifier['val']

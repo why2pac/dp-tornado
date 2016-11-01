9030b9645892cc5e7dbd11be56c25897f8b7c8c3
Parker Park <me@dp.farm>
2016-11-01 21:12:05 +0900

    implement run cli action.

--- dp_tornado/engine/template/__init__.py
+++ dp_tornado/engine/template/__init__.py
@@ -6,9 +6,15 @@ import os
 from dp_tornado import Bootstrap
 
 
-kwargs = {
-    'application_path': os.path.join(os.path.dirname(os.path.realpath(__file__))),
-}
+def run(as_cli=False):
+    kwargs = {
+        'application_path': os.path.join(os.path.dirname(os.path.realpath(__file__))),
+        'as_cli': as_cli
+    }
 
-bootstrap = Bootstrap()
-bootstrap.run(**kwargs)
+    bootstrap = Bootstrap()
+    bootstrap.run(**kwargs)
+
+
+if __name__ == '__main__':
+    run()

cf1a1822e465d5adc1c76dcbbe2fb09ecb6a5874
Parker Park <me@dp.farm>
2016-11-02 03:31:40 +0900

    example/__init__.py, cli supported style.

--- example/__init__.py
+++ example/__init__.py
@@ -6,18 +6,23 @@ import os
 from dp_tornado import Bootstrap
 
 
-kwargs = {
-    'application_path': os.path.join(os.path.dirname(os.path.realpath(__file__))),
-    'service': [
-        #(r"/post/multipart/(.*)", 'controller.post.multipart.MultipartHandler'),
-    ],
-    'scheduler': [
-        ('22 16 * * *', 'scheduler.foo'),
-        ('* * * * *', 'scheduler.tests.periodic.iter_min_repeat_2', {'repeat': 1}),
-        (1, 'scheduler.tests.periodic.iter_2_repeat_2', {'clone': 2, 'repeat': 2}),
-        (1, 'scheduler.tests.periodic.iter_1_repeat_1', {'clone': 1, 'repeat': 1})
-    ]
-}
+def run(as_cli=False):
+    kwargs = {
+        'as_cli': as_cli,
+        'application_path': os.path.join(os.path.dirname(os.path.realpath(__file__))),
+        'service': [
+            #(r"/post/multipart/(.*)", 'controller.post.multipart.MultipartHandler'),
+        ],
+        'scheduler': [
+            ('* * * * *', 'scheduler.tests.periodic.iter_min_repeat_2', {'repeat': 1}),
+            (1, 'scheduler.tests.periodic.iter_2_repeat_2', {'clone': 2, 'repeat': 2}),
+            (1, 'scheduler.tests.periodic.iter_1_repeat_1', {'clone': 1, 'repeat': 1})
+        ]
+    }
 
-bootstrap = Bootstrap()
-bootstrap.run(**kwargs)
+    bootstrap = Bootstrap()
+    bootstrap.run(**kwargs)
+
+
+if __name__ == '__main__':
+    run()

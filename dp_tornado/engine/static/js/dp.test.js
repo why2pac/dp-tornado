2f7611c10d190910aabaff2a0ec9797b6be8e66b
Parker Park <me@dp.farm>
2016-11-07 15:05:39 +0900

    implement dp.ui.element.input.delegate helper.

--- dp_tornado/engine/static/js/dp.test.js
+++ dp_tornado/engine/static/js/dp.test.js
@@ -1,20 +1,90 @@
 if (!dp) var dp = {};
 
 dp.test = {
+    failed: false,
     init: function() {
+        var wait = 10;
+
+        dp_assert(function() {
+            return _el('#input-test').length;
+        }, 'test-el');
+
         dp_assert(dp.helper.string.uniqid(), 'test-uniqid');
         dp_assert(dp.helper.string.uniqid('prefix_'), 'test-uniqid-with-prefix');
         dp_assert(dp.helper.string.uniqid('prefix_', true), 'test-uniqid-with-entropy');
         dp_assert(dp.helper.string.uniqid('prefix_', true, '-'), 'test-uniqid-with-entropy-separator');
 
-        dp_assert(true, 'all-test-cases');
+        dp_assert(
+            function() {
+                return dp.test.ui.element.input.delegate.on_focus_called;
+            },
+            'test-ui-element-input-delegate-on-focus',
+            function() {
+                _el('#input-test').focus();
+            },
+            wait
+        );
+
+        dp_assert(
+            function() {
+                return dp.test.ui.element.input.delegate.on_return_called;
+            },
+            'test-ui-element-input-delegate-on-return',
+            function() {
+                var e = dp_jqlib.Event('keypress');
+                e.keyCode = 13;
+
+                _el('#input-test').trigger(e);
+            },
+            wait
+        );
+
+        setTimeout(function() {
+            dp_assert(!dp.test.failed, 'all-test-cases');
+        }, wait * 2);
+    },
+    ui: {
+        element: {
+            input: {
+                delegate: {
+                    on_return_called: false,
+                    on_return: function() {
+                        dp.test.ui.element.input.delegate.on_return_called = true;
+                        console.log('* dp.test.ui.element.input.delegate.on_return called.');
+                    },
+                    on_focus_called: false,
+                    on_focus: function() {
+                        dp.test.ui.element.input.delegate.on_focus_called = true;
+                        console.log('* dp.test.ui.element.input.delegate.on_focus called.');
+                    }
+                }
+            }
+        }
     }
 };
 
-var dp_assert = function(eq, identifier) {
+var dp_assert = function(eq, identifier, before, delay) {
+    if (before) {
+        before();
+    }
+
+    if (delay) {
+        setTimeout(function() {
+            dp_assert(eq, identifier);
+        }, delay);
+        return;
+    }
+
+    if (typeof eq == 'function') {
+        eq = eq();
+    }
+
     if (!eq) {
+        if (dp.test.failed) return;
+
         console.log('* Assertion Failed, ' + identifier);
         dp_jqlib('body').append(dp_jqlib('<p />').addClass('assert').addClass('fail').text('* Assertion Failed, ' + identifier));
+        dp.test.failed = true;
 
         throw new Error('* Assertion Failed, ' + identifier);
     }
--- dp_tornado/engine/static/js/dp.ui.js
+++ dp_tornado/engine/static/js/dp.ui.js
@@ -1,5 +1,68 @@
 if (!dp) var dp = {};
 
 dp.ui = {
+    _initiated: false,
+    init: function(obj) {
+        if (!obj && dp.ui._initiated) return;
+        dp.ui._initiated = true;
+        dp.ui.element.init(obj);
+    },
+    util: {
+        alert: function(a, b, c, d, e, f, g) {
+            alert('not implemented yet.');
+        },
+        noti: function(a, b, c, d) {
+            alert('not implemented yet.');
+        }
+    },
+    element: {
+        init: function(obj) {
+            dp.ui.element.input.delegate.on_return(obj);
+            dp.ui.element.input.delegate.on_focus(obj);
+        },
+        input: {
+            delegate: {
+                on_return: function(obj) {
+                    if (!obj) obj = dp_jqlib('body');
+                    obj.find('input[dp-on-return][dp-on-return-installed!=yes]').each(function() {
+                        var _id = dp_jqlib(this).attr('id') || 'uniqid-' + dp.helper.string.uniqid();
 
+                        dp_jqlib(this).attr('id', _id);
+                        dp_jqlib(this).attr('dp-on-return-installed', 'yes');
+
+                        dp_jqlib(this).keypress(function(e) {
+                            if (dp_jqlib(this).attr('dp-on-return-busy') == 'yes') {
+                                return;
+                            }
+
+                            if (e.keyCode == 13) {
+                                dp_jqlib(this).attr('dp-on-return-busy', 'yes');
+                                setTimeout(dp_jqlib(this).attr('dp-on-return'), 0);
+                                setTimeout("dp_jqlib('#" + _id + "').attr('dp-on-return-busy', 'no');", 150);
+
+                                e.preventDefault();
+                            }
+                        });
+                    });
+                },
+                on_focus: function(obj) {
+                    if (!obj) obj = dp_jqlib('body');
+                    obj.find('input[dp-on-focus][dp-on-focus-installed!=yes]').each(function() {
+                        var _id = dp_jqlib(this).attr('id') || 'uniqid-' + dp.helper.string.uniqid();
+
+                        dp_jqlib(this).attr('id', _id);
+                        dp_jqlib(this).attr('dp-on-focus-installed', 'yes');
+
+                        dp_jqlib(this).focus(function(e) {
+                            setTimeout(dp_jqlib(this).attr('dp-on-focus'), 0);
+                        });
+                    });
+                }
+            }
+        }
+    }
 };
+
+dp_init(function() {
+    dp.init();
+});

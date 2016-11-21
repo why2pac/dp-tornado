973322e3f0db087c93f607f207f2fa2bc6f6cd92
Parker Park <me@dp.farm>
2016-11-21 17:57:05 +0900

    remove unnecessary interface methods.

--- dp_tornado/engine/schema/driver/__init__.py
+++ dp_tornado/engine/schema/driver/__init__.py
@@ -9,19 +9,3 @@ class SchemaDriver(object):
     @staticmethod
     def migrate_data(dsn, table):
         raise NotImplementedError
-
-    @staticmethod
-    def migrate_fields(proxy, table, fields, exist):
-        raise NotImplementedError
-
-    @staticmethod
-    def _migrate_fields_create(proxy, table, fields, exist):
-        raise NotImplementedError
-
-    @staticmethod
-    def migrate_indexes(proxy, table, indexes, exist):
-        raise NotImplementedError
-
-    @staticmethod
-    def migrate_foreign_keys(proxy, table, foreign_keys, exist):
-        raise NotImplementedError

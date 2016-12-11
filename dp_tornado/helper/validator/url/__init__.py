b21e2a252fbb513df6713d6218aa1bb460601abe
Parker Park <me@dp.farm>
2016-12-11 17:53:34 +0900

    add string validation.

--- dp_tornado/helper/validator/url/__init__.py
+++ dp_tornado/helper/validator/url/__init__.py
@@ -8,4 +8,4 @@ from dp_tornado.engine.helper import Helper as dpHelper
 
 class UrlHelper(dpHelper):
     def validate(self, url):
-        return True if validators.url(url) is True else False
+        return True if self.helper.misc.type.check.string(url) and validators.url(url) is True else False

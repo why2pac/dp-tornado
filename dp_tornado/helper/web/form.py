5c1528d6018563c2f0fa7b64c515e66250113c17
Parker Park <me@dp.farm>
2017-01-02 04:07:39 +0900

    fixed alias method parameter mismatch.

--- dp_tornado/helper/web/form.py
+++ dp_tornado/helper/web/form.py
@@ -5,5 +5,5 @@ from dp_tornado.engine.helper import Helper as dpHelper
 
 
 class FormHelper(dpHelper):
-    def validate(self, e):
-        return self.helper.validator.form.validate(e)
+    def validate(self, controller, fields, error_res='json'):
+        return self.helper.validator.form.validate(controller, fields, error_res)
